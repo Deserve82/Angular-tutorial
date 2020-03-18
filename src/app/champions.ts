314512bb17703196c68e69155be75ca77b9815e4
Kangho Lee <inth8204@gamil.com>
2020-03-18 23:33:16 +0900

    add2

--- src/app/champions.ts
+++ src/app/champions.ts
@@ -1,13 +1,31 @@
 
 export const champs = [
   { id: 1,
-    name: 'Biltz-Crank',
+    name: '무서운 이야기 1',
     skill: 'Thunder killer',
     image: 'assets/profile/asdf.jpeg',
     story: 'She is kind and strong'
   },
   { id: 2,
-    name: 'Fiddle-Stick',
+    name: '무서운 이야기 2',
+    skill: 'Horrify kids',
+    image: 'assets/profile/fiddle.jpeg',
+    story: 'fiddle stick cosume little soul'
+  },
+    { id: 2,
+    name: '무서운 이야기 2',
+    skill: 'Horrify kids',
+    image: 'assets/profile/fiddle.jpeg',
+    story: 'fiddle stick cosume little soul'
+  },
+    { id: 2,
+    name: '무서운 이야기 2',
+    skill: 'Horrify kids',
+    image: 'assets/profile/fiddle.jpeg',
+    story: 'fiddle stick cosume little soul'
+  },
+    { id: 2,
+    name: '무서운 이야기 2',
     skill: 'Horrify kids',
     image: 'assets/profile/fiddle.jpeg',
     story: 'fiddle stick cosume little soul'
